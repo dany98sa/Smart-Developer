f0555cf12e609f9eb4b8312e412e02ab9f71af2b
smarttesting98 <demarcodaniele98@gmail.com>
2022-05-04 02:41:25 -0700

    test: generate test case with evosuite

--- generateTest/evosuiteRun.js
+++ generateTest/evosuiteRun.js
@@ -25,7 +25,7 @@ exports.generateTest = (repo) => {
     logger.info("mvn compile " + repo)
     //se il progetto è stato compilato esegue evosuite
 
-        const evosuiteOutput = shell.exec('$EVOSUITE -target target/classes')
+        const evosuiteOutput = shell.exec('$EVOSUITE $EVOSUITE -target target/classes')
 
         if (evosuiteOutput.code !== 0) {
             logger.error("evosuite compile error")
